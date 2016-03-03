12422708bf0931ae97b2c5d4e5f248cb6d177e6b
Trisha Sengupta <tsengupta64@gmail.com>
2016-03-02 23:30:58 -0600

    Added files via upload

---
+++ JLetter.java
@@ -0,0 +1,27 @@
+/** 
+ * Prints the letter J as a large block letter
+ * consisting of the phrase JAVA.
+ *
+ * @author Trisha Sengupta
+ * @version 08-31-2015
+ */
+ 
+public class JLetter {
+   /**
+    * Prints letter J.
+    *
+    * @param args Command line arguments (not used).
+    */
+   public static void main(String[] args) {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
---
+++ MyLifeGoals.java
@@ -0,0 +1,29 @@
+/** 
+ * Prints your name, your short-term, medium-term
+ * long term goals.
+ *
+ * @author Trisha Sengupta
+ * @version 08-31-2015
+ */
+
+public class MyLifeGoals {
+   /**
+    * Prints life goals to std output.
+    *
+    * @param args Command line arguments (not used).
+   */
+   public static void main(String[] args) {
+      System.out.println("Trisha Sengupta");
+      System.out.println();
+      System.out.println("My short-term goals are to do my " 
+         + "physics and calculus homework this week and " 
+         + "study for both of those classes since I have " 
+         + "an upcoming quiz and test.");
+      System.out.println("My medium-term goals are to " 
+         + "successfully get a co-op and build up the" 
+         + "experience I need in order to develop admirable skills.");
+      System.out.println("My long-term goal is to graduate " 
+         + "from college stress-free by already having a job " 
+         + "or being hired right after graduation.");
+   }
+}
